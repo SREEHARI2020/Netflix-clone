c5ac4f57db247beb4e70bb5435bdd2a4adcaabd7
SREEHARI <sreehari2610@gmail.com>
2020-12-28 16:50:01 +0530

    movie-posters-initial

--- src/Row.js
+++ src/Row.js
@@ -1,24 +1,29 @@
 import React,{useState,useEffect} from 'react'
-import axios from './axios'
+import axios from './axios';
 
 const Row = ({title,fetchUrl}) => {
+    const base_url = "https://image.tmdb.org/t/p/original/"; //for images
     const [movies, setMovies] = useState([])
     useEffect(()=>{
 
         const fetchData=async()=>{
             const request= await axios.get(fetchUrl);
-            console.log(request)
+            console.table(request.data.results)
 
-            setMovies(request);
+            setMovies(request.data.results);
         }
         fetchData();
 
-    },[])
+    },[fetchUrl])
 
 
     return (
-        <div>
-            {title}
+        <div className="row">
+           <h2>{title}</h2>
+           <div className="row_posters">
+               {movies.map((movie)=>(
+               <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>))}
+           </div>
         </div>
     )
 }
