20f550a5779b6c357180504d9a5276ea9c287607
SREEHARI <sreehari2610@gmail.com>
2020-12-28 18:43:34 +0530

    movies-categories

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ function App() {
     <div className="App">
     <h1>Netflix-clone</h1>
     <Row title="Netflix Orginals" fetchUrl={requests.fetchNetflixOriginals}/>
-    <Row title="trending now" fetchUrl={requests. fetchTrending}/>
+    <Row title="Trending Now" fetchUrl={requests. fetchTrending}/>
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+    <Row title="Action Movies" fetchUrl={requests. fetchActionMovies}/>
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+    <Row title="Romance Movies" fetchUrl={requests.  fetchRomanceMovies}/>
+    <Row title="Documentaries" fetchUrl={requests. fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -23,7 +23,7 @@ const Row = ({title,fetchUrl}) => {
             <h2>{title}</h2>
              <div className="row_posters">
                  {movies.map((movie)=>(
-               <img className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>))}
+               <img key={movie.id} className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>))}
            </div>
         </div>
     )
